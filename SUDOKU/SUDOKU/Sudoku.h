9a1bd81668a891460d6a1843ed0b0633a7dd4862
Romeo-Colleghi <125869374+Romeo-Colleghi@users.noreply.github.com>
2023-05-02 10:52:19 +0200

    bozza1

---
+++ SUDOKU/SUDOKU/Sudoku.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+#define righe 9;
+#define colonnee 9;
+
+using namespace std;
+
+class Sudoku
+{
+private:
+
+	int Quadratone[righe][colonnee];
+
+public:
+
+
+};
+
