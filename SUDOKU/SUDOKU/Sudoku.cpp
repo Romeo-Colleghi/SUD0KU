3369934917a61f14d7cdd7425bb1c1448656dc4c
Romeo-Colleghi <125869374+Romeo-Colleghi@users.noreply.github.com>
2023-05-09 11:16:03 +0200

    aspo

--- SUDOKU/SUDOKU/Sudoku.cpp
+++ SUDOKU/SUDOKU/Sudoku.cpp
@@ -1,1 +1,25 @@
 #include "Sudoku.h"
+
+bool Sudoku::TrovatoRiga(int num, int numRiga)
+{
+	bool cond = 0;
+
+	for (int i = 0; i < colonnee; i++)
+	{
+		if (Sudoku[numRiga][i])
+		{
+
+		}
+
+	}
+}
+
+bool Sudoku::TrovatoColonna(int num, int numColonna)
+{
+	return false;
+}
+
+bool Sudoku::TrovatoQuadrato(int num, int numRiga, int numColonna)
+{
+	return false;
+}
--- SUDOKU/SUDOKU/Sudoku.h
+++ SUDOKU/SUDOKU/Sudoku.h
@@ -11,9 +11,24 @@ class Sudoku
 {
 private:
 
-	int Quadratone[righe][colonnee];
+	int Sudoku[righe][colonnee] = {
+		{0,9,3,0,5,4,0,0,0},
+		{0,0,0,9,2,0,0,4,0},
+		{2,0,0,0,0,0,8,0,0},
+		{3,8,6,1,9,0,0,7,4},
+		{0,0,0,0,0,3,0,8,1},
+		{4,0,9,5,7,8,0,6,2},
+		{0,2,8,7,0,0,4,0,0},
+		{0,0,0,0,0,5,0,9,0},
+		{9,0,1,0,0,0,7,3,0}
+
+	};
 
 public:
+
+	bool TrovatoRiga(int num, int numRiga);
+	bool TrovatoColonna(int num, int numColonna);
+	bool TrovatoQuadrato(int num, int numRiga, int numColonna);
 
 
 };
